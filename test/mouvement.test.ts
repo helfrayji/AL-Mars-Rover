be55b17364976da7d2b44f10507f270b045cd061
iliesskalli <58472432+iliesskalli@users.noreply.github.com>
2024-02-22 14:17:01 +0100

    Update mouvement.test.ts

--- test/mouvement.test.ts
+++ test/mouvement.test.ts
@@ -36,10 +36,11 @@ describe("Rover movements", () => {
         rover = rover.moveBackward();
 
         // Then
-        expect(rover.position).toEqual(new Point(0, 359)); // Assuming modulo 360 operation
+        expect(rover.position).toEqual(new Point(0, 359));
     });
 
     test("Given rover is facing North when it turns left then it should face West", () => {
+
         // Given
         const initialOrientation = Orientation.North;
         rover = new Rover(new Point(0, 0), initialOrientation, planet);
@@ -52,7 +53,7 @@ describe("Rover movements", () => {
     });
 
     test("Given rover is facing North when it turns right then it should face East", () => {
-        
+
         // Given
         const initialOrientation = Orientation.North;
         rover = new Rover(new Point(0, 0), initialOrientation, planet);
@@ -76,6 +77,70 @@ describe("Rover movements", () => {
         // When
         rover = rover.moveForward();
 
+        // Then
+        expect(rover.position).toEqual(initialPosition);
+    });
+
+    test("Given rover is facing East when it moves forward then it should move one unit eastward", () => {
+        // Given
+        const initialPosition = new Point(0, 0);
+        rover = new Rover(initialPosition, Orientation.East, planet);
+
+        // When
+        rover = rover.moveForward();
+
+        // Then
+        expect(rover.position).toEqual(new Point(1, 0));
+    });
+
+    test("Given rover is facing West when it moves backward then it should move one unit westward", () => {
+        // Given
+        const initialPosition = new Point(3, 3);
+        rover = new Rover(initialPosition, Orientation.West, planet);
+
+        // When
+        rover = rover.moveBackward();
+
+        // Then
+        expect(rover.position).toEqual(new Point(4, 3));
+    });
+
+    test("Given rover is facing South when it turns left then it should face East", () => {
+        // Given
+        const initialOrientation = Orientation.South;
+        rover = new Rover(new Point(0, 0), initialOrientation, planet);
+
+        // When
+        rover = rover.turnLeft();
+
+        // Then
+        expect(rover.orientation).toEqual(Orientation.East);
+    });
+
+    test("Given rover is facing South when it turns right then it should face West", () => {
+        // Given
+        const initialOrientation = Orientation.South;
+        rover = new Rover(new Point(0, 0), initialOrientation, planet);
+
+        // When
+        rover = rover.turnRight();
+
+        // Then
+        expect(rover.orientation).toEqual(Orientation.West);
+    });
+
+
+    test("Given rover encounters an obstacle when it moves backward then it should stay in the same position", () => {
+        // Given
+        const obstaclePosition = new Point(0, 359);
+        const obstacle = new Obstacle(obstaclePosition);
+        planet.addObstacle(obstacle);
+        const initialPosition = new Point(0, 0);
+        rover = new Rover(initialPosition, Orientation.North, planet);
+    
+        // When
+        rover = rover.moveBackward();
+    
         // Then
         expect(rover.position).toEqual(initialPosition);
     });
