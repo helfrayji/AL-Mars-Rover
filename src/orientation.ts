3166a56a30ac1b82b059840880e0360bedac31d8
helfrayji <82065473+helfrayji@users.noreply.github.com>
2024-02-23 14:44:29 +0100

    Update orientation.ts

--- src/orientation.ts
+++ src/orientation.ts
@@ -2,90 +2,46 @@ import { Point } from "./point";
 import { Planet } from "./planet";
 
 export class Orientation {
-    static readonly NORTH: Orientation = new Orientation('N');
-    static readonly EAST: Orientation = new Orientation('E');
-    static readonly SOUTH: Orientation = new Orientation('S');
-    static readonly WEST: Orientation = new Orientation('W');
+    static readonly NORTH: Orientation = new Orientation('N', 0, 1);
+    static readonly EAST: Orientation = new Orientation('E', 1, 0);
+    static readonly SOUTH: Orientation = new Orientation('S', 0, -1);
+    static readonly WEST: Orientation = new Orientation('W', -1, 0);
 
     private value: string;
+    private dx: number;
+    private dy: number;
 
-    private constructor(value: string) {
+    private constructor(value: string, dx: number, dy: number) {
         this.value = value;
+        this.dx = dx;
+        this.dy = dy;
     }
 
     getValue(): string {
         return this.value;
     }
 
+    private static orientations = [Orientation.NORTH, Orientation.EAST, Orientation.SOUTH, Orientation.WEST];
+
     rotateLeft(): Orientation {
-        switch (this.value) {
-            case 'N':
-                return Orientation.WEST;
-            case 'W':
-                return Orientation.SOUTH;
-            case 'S':
-                return Orientation.EAST;
-            case 'E':
-                return Orientation.NORTH;
-            default:
-                throw new Error('Invalid orientation');
-        }
+        const index = Orientation.orientations.findIndex(o => o.value === this.value);
+        const newIndex = (index + 3) % Orientation.orientations.length; // Adding 3 is equivalent to rotating left (or subtracting 1 with wraparound)
+        return Orientation.orientations[newIndex];
     }
 
     rotateRight(): Orientation {
-        switch (this.value) {
-            case 'N':
-                return Orientation.EAST;
-            case 'E':
-                return Orientation.SOUTH;
-            case 'S':
-                return Orientation.WEST;
-            case 'W':
-                return Orientation.NORTH;
-            default:
-                throw new Error('Invalid orientation');
-        }
+        const index = Orientation.orientations.findIndex(o => o.value === this.value);
+        const newIndex = (index + 1) % Orientation.orientations.length; // Simple right rotation
+        return Orientation.orientations[newIndex];
     }
 
     moveForward(position: Point, planet: Planet): Point {
-        let newPosition = new Point(position.getLongitude(), position.getLatitude());
-
-        switch (this.value) {
-            case 'N':
-                newPosition = newPosition.incrementLatitude(1);
-                break;
-            case 'E':
-                newPosition = newPosition.incrementLongitude(1);
-                break;
-            case 'S':
-                newPosition = newPosition.DecrementLatitude(1);
-                break;
-            case 'W':
-                newPosition = newPosition.DecrementLongitude(1);
-                break;
-        }
-
+        let newPosition = new Point(position.getLongitude() + this.dx, position.getLatitude() + this.dy);
         return planet.wrapAround(newPosition);
     }
 
     moveBackward(position: Point, planet: Planet): Point {
-        let newPosition = new Point(position.getLongitude(), position.getLatitude());
-
-        switch (this.value) {
-            case 'N':
-                newPosition = newPosition.DecrementLatitude(1);
-                break;
-            case 'E':
-                newPosition = newPosition.DecrementLongitude(1);
-                break;
-            case 'S':
-                newPosition = newPosition.incrementLatitude(1);
-                break;
-            case 'W':
-                newPosition = newPosition.incrementLongitude(1);
-                break;
-        }
-
+        let newPosition = new Point(position.getLongitude() - this.dx, position.getLatitude() - this.dy);
         return planet.wrapAround(newPosition);
     }
 }
