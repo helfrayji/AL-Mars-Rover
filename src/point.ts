b2f0f6cf92ea5750f68f9c6b0ed9700927f2ed38
iliesskalli <58472432+iliesskalli@users.noreply.github.com>
2024-02-23 14:01:36 +0100

    Update point.ts

--- src/point.ts
+++ src/point.ts
@@ -1,5 +1,5 @@
 export class Point {
-    constructor(public readonly longitude: number, public readonly latitude: number) {}
+    constructor(private readonly longitude: number, private readonly latitude: number) {}
 
     incrementLongitude(amount: number): Point {
         const newLongitude = (this.longitude + amount) % 360;
@@ -19,5 +19,17 @@ export class Point {
     DecrementLatitude(amount: number): Point {
         const newLatitude = (this.latitude - amount) % 360;
         return new Point(this.longitude, newLatitude);
+    }
+
+    getLongitude(): number {
+        return this.longitude;
+    }
+
+    getLatitude(): number {
+        return this.latitude;
+    }
+
+    isAtSamePointAs(otherPoint: Point): boolean {
+        return this.longitude === otherPoint.getLongitude() && this.latitude === otherPoint.getLatitude();
     }
 }
