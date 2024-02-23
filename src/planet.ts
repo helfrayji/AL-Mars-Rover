2a10b982812af453fe37338a2cf1b75d18713679
iliesskalli <58472432+iliesskalli@users.noreply.github.com>
2024-02-23 14:01:53 +0100

    Update planet.ts

--- src/planet.ts
+++ src/planet.ts
@@ -4,25 +4,25 @@ import { Obstacle } from "./obstacle";
 const FULL_CIRCLE_DEGREES: number = 360;
 
 export class Planet {
-    private readonly obstacles: Obstacle[] = []; 
+    private obstacles: Obstacle[] = [];
 
-    constructor(public readonly radius: number) {}
+    constructor(private readonly radius: number) {}
 
     addObstacle(obstacle: Obstacle): void {
         this.obstacles.push(obstacle);
     }
 
-    isObstacle(point: Point): boolean {
-        return this.obstacles.some(obstacle => obstacle.position.longitude === point.longitude && obstacle.position.latitude === point.latitude);
+    hasObstacleAt(point: Point): boolean {
+        return this.obstacles.some(obstacle => obstacle.position.isAtSamePointAs(point));
     }
 
     wrapAround(point: Point): Point {
-        const wrappedLongitude = (point.longitude + FULL_CIRCLE_DEGREES) % FULL_CIRCLE_DEGREES;
-        const wrappedLatitude = (point.latitude + FULL_CIRCLE_DEGREES) % FULL_CIRCLE_DEGREES;
+        const wrappedLongitude = (point.getLongitude() + FULL_CIRCLE_DEGREES) % FULL_CIRCLE_DEGREES;
+        const wrappedLatitude = (point.getLatitude() + FULL_CIRCLE_DEGREES) % FULL_CIRCLE_DEGREES;
         return new Point(wrappedLongitude, wrappedLatitude);
     }
 
     getObstacles(): Obstacle[] {
-        return this.obstacles;
+        return [...this.obstacles];
     }
 }
